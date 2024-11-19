4439624110c1651d6d04bb3f360fdc140adc706a
s3Em <madhuripatil0015@gmail.com>
2024-11-19 20:19:21 +0530

    Replace powershell commmand with cmd commmand becuase powershell is slow

--- winBright.py
+++ winBright.py
@@ -7,8 +7,18 @@ print("Press <Alt>+<Shift>+a: Increase Brightness")
 
 # Updateding cuurent windows brightness #
 def getCurrentBrightness():
-    currentWindowBrightness = os.popen('powershell -Command "Get-Ciminstance -Namespace root/WMI -ClassName WmiMonitorBrightness | Select -ExpandProperty "CurrentBrightness"').read().strip()
-    return int(currentWindowBrightness)
+    command = "powercfg /q SCHEME_CURRENT SUB_VIDEO VIDEONORMALLEVEL"
+    currentWindowBrightness = os.popen(command).read().strip()
+
+    index = currentWindowBrightness.find("Current AC Power Setting Index:") + len("Current AC Power Setting Index: ")
+    hexVal = currentWindowBrightness[index : index + 10]
+
+    currentWindowBrightness = int(hexVal, 16)
+
+    # Printing brigtnesss 
+    # print(currentWindowBrightness)
+    
+    return currentWindowBrightness
 
 def adjustBrightness(currentWindowBrightness):
     if currentWindowBrightness < 0:
@@ -21,25 +31,28 @@ def adjustBrightness(currentWindowBrightness):
 # Decrease function that can decresing value #
 def increase():
     currentWindowBrightness = getCurrentBrightness()
-    currentWindowBrightness = int(currentWindowBrightness) + 1
+    currentWindowBrightness = currentWindowBrightness + 1
 
     currentWindowBrightness = adjustBrightness(currentWindowBrightness)
 
     # Brightness command that can decreasing brightness #
-    adjust_brightness_command = "powershell (Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1,{0})".format(currentWindowBrightness)
+    adjust_brightness_command = "powercfg -setdcvalueindex SCHEME_CURRENT SUB_VIDEO aded5e82-b909-4619-9949-f5d71dac0bcb {0} && powercfg /setactive SCHEME_CURRENT".format(currentWindowBrightness)
+    adjust_brightness_command = "powercfg -setacvalueindex SCHEME_CURRENT SUB_VIDEO aded5e82-b909-4619-9949-f5d71dac0bcb {0} && powercfg /setactive SCHEME_CURRENT".format(currentWindowBrightness)
     os.system(adjust_brightness_command)
 
 
 # increase function that can increase value #
 def decrease():
     currentWindowBrightness = getCurrentBrightness() # 100
-    currentWindowBrightness = int(currentWindowBrightness) - 1 # 99
+    currentWindowBrightness = currentWindowBrightness - 1 # 99
     currentWindowBrightness = adjustBrightness(currentWindowBrightness) # 99
 
     # Brightness command that can increasing brightness #
-    adjust_brightness_command = "powershell (Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1,{0})".format(currentWindowBrightness)
+    adjust_brightness_command = "powercfg -setdcvalueindex SCHEME_CURRENT SUB_VIDEO aded5e82-b909-4619-9949-f5d71dac0bcb {0} && powercfg /setactive SCHEME_CURRENT".format(currentWindowBrightness)
+    adjust_brightness_command = "powercfg -setacvalueindex SCHEME_CURRENT SUB_VIDEO aded5e82-b909-4619-9949-f5d71dac0bcb {0} && powercfg /setactive SCHEME_CURRENT".format(currentWindowBrightness)
     os.system(adjust_brightness_command)
 
 
 # Hotkeys: Handleing keyboared input #
-with keyboard.GlobalHotKeys({'<shift>+<alt>+a': increase,'<shift>+<alt>+z': decrease}) as h:h.join()
+with keyboard.GlobalHotKeys({'<shift>+<alt>+a': increase,'<shift>+<alt>+z': decrease}) as h:
+    h.join()
